f606f22dd340d0590d0160d6b1c1994d50d9d13e
unknown <matt.j.velazquez@gmail.com>
2023-08-05 12:39:42 -0700

    retooled logic to showcase today's forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,45 +38,77 @@ function displayWeatherData(data) {
     const weatherDataDiv = document.getElementById("weatherData");
     weatherDataDiv.innerHTML = "";
 
-    const forecastsByDay = {};
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
 
-    for (let i = 0; i < data.list.length; i++) {
-        const forecast = data.list[i];
-        const forecastDate = new Date(forecast.dt * 1000); 
-        const dateKey = forecastDate.toDateString();
+    const todayForecast = data.list.find((forecast) => {
+        const forecastDate = new Date(forecast.dt * 1000);
+        forecastDate.setHours(0, 0, 0, 0);
+        return forecastDate.getTime() === today.getTime();
+    });
 
-        // skip if the forecast for this day has already been displayed
-        if (forecastsByDay[dateKey]) {
-            continue;
-        }
+    const forecastRow = document.createElement("div");
+    forecastRow.classList.add("row");
 
+    if (todayForecast) {
+        const todayForecastDate = new Date(todayForecast.dt * 1000);
+        const windSpeed = todayForecast.wind.speed.toFixed(2);
+        const temperatureKelvin = todayForecast.main.temp;
+        const temperatureFahrenheit = kelvinToFahrenheit(temperatureKelvin).toFixed(2);
+        const description = todayForecast.weather[0].description;
+        const weatherIcon = `http://openweathermap.org/img/wn/${todayForecast.weather[0].icon}.png`;
+
+        const todayForecastCol = document.createElement("div");
+        todayForecastCol.classList.add("col-12", "forecast-item", "today-forecast");
+        todayForecastCol.innerHTML = `
+        <div class="card">
+            <div class="card-body">
+                <h3 class="card-title"> Today's Forecast (${todayForecastDate.toDateString()})</h3>
+                <img src="${weatherIcon}" alt-"Weather Icon">
+                <p>Temperature: ${temperatureFahrenheit} °F</p>
+                <p>Wind Speed: ${windSpeed} MPH</p>
+                <p>Weather: ${description}</p>
+            </div>
+        </div>        
+        `;
+        forecastRow.appendChild(todayForecastCol);
+    }
+
+    //Display rest of the 5 day forecast
+    const forecasts = data.list.filter((forecast) => {
+        const forecastDate = new Date(forecast.dt * 1000);
+        forecastDate.setHours(0, 0, 0, 0);
+        return forecastDate.getTime() !== today.getTime();
+    });
+
+    for (let i = 0; i < Math.min(forecasts.length, 5); i++) {
+        const forecast = forecasts[i];
+        const forecastDate = new Date(forecast.dt * 1000);
         const windSpeed = forecast.wind.speed.toFixed(2);
         const temperatureKelvin = forecast.main.temp;
-        const temperatureFahrenheit = kelvinToFahrenheit(temperatureKelvin).toFixed(2);//Convert to Fahrenheit
-        const description = forecast.weather[0].description
+        const temperatureFahrenheit = kelvinToFahrenheit(temperatureKelvin).toFixed(2);
+        const description = forecast.weather[0].description;
         const weatherIcon = `http://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`;
 
-        //create HTML elements to display forcast data
-        const forecastDiv = document.createElement("div");
-        forecastDiv.classList.add("col-sm-4", "forecast-item");
-        forecastDiv.innerHTML = `
+        //bootstrap column to dispaly forecast datat
+        const forecastCol = document.createElement("div");
+        forecastCol.classList.add("col-sm-4", "forecast-item");
+        forecastCol.innerHTML = `
         <div class="card">
-        <div class="card-body">
-        <h3 class="card-title">Date: ${forecastDate.toDateString()}</h3>
-        <img src="${weatherIcon}" alt="Weather Icon">
-        <p>Wind: ${windSpeed} MPH</p>
-        <p>Temperature: ${temperatureFahrenheit} °F</p>
-        <p>Weather: ${description}</p>
-        </div>
-        </div>
+            <div class="card-body">
+                <h3 class="card-title">${forecastDate.toDateString()}</h3>
+                <img src="${weatherIcon}" alt="Weather Icon">
+                <p>Temperature: ${temperatureFahrenheit} °F</p>
+                <p>Wind Speed: ${windSpeed} MPH</p>
+                <p>Weather: ${description}</p>
+            </div>
+        </div>        
         `;
-
-        //append forecast data to weatherDataDiv
-        weatherDataDiv.appendChild(forecastDiv);
-
-        //store this day's forecast in the forecastsByDay object
-        forecastsByDay[dateKey] = true;
+        //append forecast data to forecastRow
+        forecastRow.appendChild(forecastCol);
     }
+    //append the forecastRow to weatherDataDiv
+    weatherDataDiv.appendChild(forecastRow);
 }
 
 //save the searched city to the local client-side storage
